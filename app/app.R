6f72a64281f2918a7169f53bd799b1028dfdde71
Diana Ibishi <164220205+DianaIbishi@users.noreply.github.com>
2025-09-25 15:31:00 +0200

    Update app.R

--- app/app.R
+++ app/app.R
@@ -23,11 +23,12 @@ library(ragnar)
 # Path to the DuckDB file
 db_path <- "df_law_fin.duckdb"
 
+# Connect to the DuckDB database
+con <- dbConnect(duckdb::duckdb(), db_path, read_only = TRUE)
+
 # Connect to the DuckDB store which was created in Criminal Law Assistant_final/Embedding and chunking/Training_create_fin_v2.R
 store <- ragnar_store_connect("df_law_fin.duckdb", read_only = TRUE)   #read_only = 
 
-# Connect to the DuckDB database
-con <- dbConnect(duckdb::duckdb(), db_path, read_only = TRUE)
 
 # 3. Define system_prompt -------------------------------------------------
 
@@ -220,6 +221,7 @@ server <- function(input, output, session) {
 
 # Run the application 
 shinyApp(ui = ui, server = server)
+
 
 
 
